2c03c2dbc1018bb1e23e5cb4348275207fc843bb
Ben Gimblett <begim@microsoft.com>
2019-12-24 10:33:12 +0000

    update func code

--- PSHttpTriggerDemo/run.ps1
+++ PSHttpTriggerDemo/run.ps1
@@ -23,36 +23,72 @@ else {
     $body = "Please pass a container name on the query string or in the request body."
 }
 
+# try and get blob list for this container/storage acc
+$xmlContent = ""
+$utf8Bom = $false
+$blobCount = -1
+$status = [HttpStatusCode]::BadRequest
 
-# assumes the MSI has been given blob permission on the storage acc (not contrib and owner are not enough)
-$resourceURI = "https://storage.azure.com/"
-$tokenAuthURI = $env:MSI_ENDPOINT + "?resource=$resourceURI&api-version=2017-09-01"
-$tokenResponse = Invoke-RestMethod -Method Get -Headers @{"Secret"="$env:MSI_SECRET"} -Uri $tokenAuthURI
-$accessToken = $tokenResponse.access_token
+try{
+    # assumes the MSI has been given blob permission on the storage acc (not contrib and owner are not enough)
+    $resourceURI = "https://storage.azure.com/"
+    $tokenAuthURI = $env:MSI_ENDPOINT + "?resource=$resourceURI&api-version=2017-09-01"
+    $tokenResponse = Invoke-RestMethod -Method Get -Headers @{"Secret"="$env:MSI_SECRET"} -Uri $tokenAuthURI
+    $accessToken = $tokenResponse.access_token
 
-#Write-Host "token $accessToken"
+    $headers = @{
+        'x-ms-version'='2017-11-09'
+        'Accept-Charset'='utf-8'
+        'Authorization'='Bearer ' + $accessToken
+    }
+    $url = "https://$accName.blob.core.windows.net/$containerName" + "?restype=container&comp=list"
+    Write-Host $url
+    $xml=""
+    $xmlcontent=Invoke-RestMethod -Uri $url -Method GET -Headers $headers 
 
-$headers = @{
-    'x-ms-version'='2017-11-09'
-    'Authorization'='Bearer ' + $accessToken
+
+} catch {
+    $body = "An unexpected Error occured calling Blob storage service"
+
 }
-$url = "https://$accName.blob.core.windows.net/$containerName" +"?restype=container&comp=list"
-$respXml=Invoke-RestMethod -Uri $url -Method GET -Headers $headers  | ConvertTo-Xml
 
-$status = [HttpStatusCode]::OK
-$body = "Storage response $resp"
+#check for bom
+if ($xmlcontent -and $xmlcontent.Length -gt 3) {
+    [byte]$char1 = [System.Convert]::ToByte([char]$xmlcontent.Substring(0,1))
+    [byte]$char2 = [System.Convert]::ToByte([char]$xmlcontent.Substring(1,1))
+    [byte]$char3 = [System.Convert]::ToByte([char]$xmlcontent.Substring(2,1))
 
-Write-Host $resp
+    ## UTF8 BOM 239 187 191 - storage service appears to add an explicit utf8 bom 
+    # Ã¯Â»Â¿<?xml version="1.0" encoding="utf-8"?>
+    if ( $char1 -eq 239 -and $char2 -eq 187 -and $char3 -eq 191 ){
+        write-host "encoding is utf8"
+        $utf8Bom = $true
+    }
+    # bom must be removed first to parse xml (afaik reason is the encoding is provided explicitly in the xml header)
+    # eg. <?xml version="1.0" encoding="utf-8"?>
+    if ( $utf8Bom -eq $true ) {
+        write-host "strip bom"
+        $xmlContent = $xmlContent.Substring(3,$xmlContent.Length-3)
+    }
+    try {
+        $xdoc = New-Object -TypeName System.Xml.XmlDocument
+        $xdoc.LoadXml($xmlContent)
+        $blobCount=$xdoc.DocumentElement.Blobs.ChildNodes.Count
+        write-host "blobs found $blobCount"
 
-$info = $respXml.SelectNodes('/blobs').count
+        $status = [HttpStatusCode]::OK
+        $body = "$blobCount"
+    }
+    catch {
+        $body = "An unexpected Error occured parsing Blob storage xml response1"
+    }
+}
+else {
+    $body = "No valid xml response from blob storage service for the input"
+}
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = $status
-    Body = "Count of blobs $info"
+    Body = $body
 })
-
-
-<?xml version="1.0" encoding="utf-8"?>
-<EnumerationResults ServiceEndpoint="https://begimreadfromdemo.blob.core.windows.net/" ContainerName="demo">
-<Blobs><Blob><Name>deleteme.txt</Name><Properties><Creation-Time>Fri, 20 Dec 2019 17:14:15 GMT</Creation-Time><Last-Modified>Fri, 20 Dec 2019 17:14:15 GMT</Last-Modified><Etag>0x8D785700AC7E733</Etag><Content-Length>4</Content-Length><Content-Type>text/plain</Content-Type><Content-Encoding /><Content-Language /><Content-MD5 /><Cache-Control /><Content-Disposition /><BlobType>BlockBlob</BlobType><AccessTier>Hot</AccessTier><AccessTierInferred>true</AccessTierInferred><LeaseStatus>unlocked</LeaseStatus><LeaseState>available</LeaseState><ServerEncrypted>true</ServerEncrypted></Properties></Blob></Blobs><NextMarker /></EnumerationResults>
